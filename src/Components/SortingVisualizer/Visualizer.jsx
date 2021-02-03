36867085d558b509117509586761f5ded3369587
ishan bharadwaj <ishan19731@gmail.com>
2021-02-03 21:04:03 +0530

    Bubble Sort Visualization added

--- src/Components/SortingVisualizer/Visualizer.jsx
+++ src/Components/SortingVisualizer/Visualizer.jsx
@@ -4,6 +4,7 @@ import Navbar from '../Navbar/Navbar';
 import BarComponent from '../BarContainer/BarContainer';
 import './Visualizer.css';
 import mergeSortAnimations from '../../Algorithms/mergeSortAnimations'
+import bubbleSortAnimations from '../../Algorithms/BubbleSortAnimations'
 
 export default class SortingVisualizer extends Component {
     constructor(props) {
@@ -12,8 +13,9 @@ export default class SortingVisualizer extends Component {
         this.state = {
             array: [],
             arrSize: 10,
-            speed: 200,
-            activeColor: "#0EF0FB"
+            speed: 100,
+            activeColor: "#0EF0FB",
+            butIndex: 0,
         };
 
         this.divRef= React.createRef();
@@ -31,12 +33,13 @@ export default class SortingVisualizer extends Component {
             array.push(randomHeight(10, 250));
         }
         this.setState({array, activeColor: "#0EF0FB"});
-        document.getElementById('mergeS').classList.remove('activeBut');
-        document.getElementById('bubbleS').classList.remove('activeBut');
-        document.getElementById('insertionS').classList.remove('activeBut');
-        document.getElementById('selectionS').classList.remove('activeBut');
-        document.getElementById('quickS').classList.remove('activeBut');
-        document.getElementById('heapS').classList.remove('activeBut');
+        document.getElementsByClassName('remove')[this.state.butIndex].classList.remove('activeBut');
+        // document.getElementById('mergeS').classList.remove('activeBut');
+        // document.getElementById('bubbleS').classList.remove('activeBut');
+        // document.getElementById('insertionS').classList.remove('activeBut');
+        // document.getElementById('selectionS').classList.remove('activeBut');
+        // document.getElementById('quickS').classList.remove('activeBut');
+        // document.getElementById('heapS').classList.remove('activeBut');
 
         // var button= document.getElementsByClassName("sort-button");
         // button.classList.remove('activeBut');
@@ -58,19 +61,65 @@ export default class SortingVisualizer extends Component {
 
 
     bubble_sort = () => {
+        this.setState({
+            butIndex: 1
+        })
+        let animations= bubbleSortAnimations([...this.state.array]);
         document.getElementById('bubbleS').classList.add('activeBut');
+        // let bubble= bubbleSortAnimations(this.state.array);
+        // this.setState ({
+        //     array: bubble
+        // })
+
+        console.log(animations)
+        
+        for(let i= 0; i< animations.length; i++) {
+            
+            const isColorChange = i%4<2;
+
+            // const isColorChange= i%4<2 ? true : false;
+            
+            setTimeout(() => {
+                if(isColorChange) {
+                    const[firstBar, secondBar] = animations[i];
+                    let color= i%4===0 ? 'palevioletred' : '#0EF0FB';
+                        document.getElementsByClassName('bar')[firstBar].style.backgroundColor= color;
+                        document.getElementsByClassName('bar')[secondBar].style.backgroundColor= color;
+                }
+                
+                else {
+                    if(animations[i].length>1) {
+                            const [firstBar, newHeight] = animations[i];
+                            console.log(newHeight)
+                            document.getElementsByClassName('bar')[firstBar].style.height = `${newHeight}px`;
+                    }
+                }
+            }, i*this.state.speed );
+        }
+        setTimeout(this.sortComplete.bind(this), animations.length*this.state.speed)
+        
+        
     }
 
     selection_sort = () => {
+        this.setState({
+            butIndex: 2
+        })
         document.getElementById('selectionS').classList.add('activeBut');
     }
 
     insertion_sort = () => {
+        this.setState({
+            butIndex: 3
+        })
         document.getElementById('insertionS').classList.add('activeBut');
     }
     
 
     merge_sort = () => {
+        // this.setState({
+        //     butIndex: 4
+        // })
         const animations = mergeSortAnimations(this.state.array);
         console.log(animations.length);
         document.getElementById('mergeS').classList.add('activeBut');
@@ -108,10 +157,16 @@ export default class SortingVisualizer extends Component {
     }
 
     quick_sort = () => {
+        this.setState({
+            butIndex: 5
+        })
         document.getElementById('quickS').classList.add('activeBut');
     }
 
     heap_sort = () => {
+        this.setState({
+            butIndex: 6
+        })
         document.getElementById('heapS').classList.add('activeBut');
     }
 
@@ -130,12 +185,12 @@ export default class SortingVisualizer extends Component {
             <div className="stage"></div>
             <BarComponent array={this.state.array} barColor={this.state.activeColor}/>        
             <div className="but-wrap">
-                <div className="sort-button rem" id="bubbleS" tabindex="1" onClick={this.bubble_sort}>Bubble Sort</div>
-                <div className="sort-button rem" id="selectionS" tabindex="2" onClick={this.selection_sort}>Selection Sort</div>
-                <div className="sort-button rem" id="insertionS" tabindex="3" onClick={this.insertion_sort}>Insertion Sort</div>
-                <div className="sort-button rem" id="mergeS" tabindex="4" onClick={this.merge_sort} /*ref={this.divRef}*/>Merge Sort</div>
-                <div className="sort-button rem" id="quickS" tabindex="5" onClick={this.quick_sort}>Quick Sort</div>
-                <div className="sort-button rem" id="heapS" tabindex="6" onClick={this.heap_sort}>Heap Sort</div>
+                <div className="sort-button remove" id="bubbleS" tabindex="1" onClick={this.bubble_sort}>Bubble Sort</div>
+                <div className="sort-button remove" id="selectionS" tabindex="2" onClick={this.selection_sort}>Selection Sort</div>
+                <div className="sort-button remove" id="insertionS" tabindex="3" onClick={this.insertion_sort}>Insertion Sort</div>
+                <div className="sort-button remove" id="mergeS" tabindex="4" onClick={this.merge_sort} /*ref={this.divRef}*/>Merge Sort</div>
+                <div className="sort-button remove" id="quickS" tabindex="5" onClick={this.quick_sort}>Quick Sort</div>
+                <div className="sort-button remove" id="heapS" tabindex="6" onClick={this.heap_sort}>Heap Sort</div>
             </div>
             </div>
             </div>
